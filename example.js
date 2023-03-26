3abe1faceb814b3eb1c5bb33283b2ec934778d01
surajit-94 <surajitripathy@gmail.com>
2023-03-26 08:16:07 +0530

    first committ

---
+++ example.js
@@ -0,0 +1,22 @@
+//get element by class name
+// var items=document.getElementsByClassName("list-group-item");
+// console.log(items);
+// console.log(items[1]);
+// items[1].style.fontWeight="bold";
+// items[1].style.backgroundColor="yellow";
+
+// for(let i=0; i<items.length ; i++)
+// {
+//     items[i].style.backgroundColor="#f4f4f4";
+// }
+
+var li=document.getElementsByTagName("li");
+console.log(li);
+console.log(li[1]);
+// li[1].style.fontWeight="bold";
+li[2].style.backgroundColor="yellow";
+
+for(let i=0; i<li.length ; i++)
+{
+    li[i].style.fontWeight="bold";
+}
